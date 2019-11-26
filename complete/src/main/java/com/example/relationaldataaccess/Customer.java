d8aa3e41da19982a59029150f936944fee03874c
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:09:00 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/com/example/relationaldataaccess/Customer.java
+++ complete/src/main/java/com/example/relationaldataaccess/Customer.java
@@ -1,21 +1,21 @@
 package com.example.relationaldataaccess;
 
 public class Customer {
-    private long id;
-    private String firstName, lastName;
+	private long id;
+	private String firstName, lastName;
 
-    public Customer(long id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+	public Customer(long id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 
-    @Override
-    public String toString() {
-        return String.format(
-                "Customer[id=%d, firstName='%s', lastName='%s']",
-                id, firstName, lastName);
-    }
+	@Override
+	public String toString() {
+		return String.format(
+				"Customer[id=%d, firstName='%s', lastName='%s']",
+				id, firstName, lastName);
+	}
 
-    // getters & setters omitted for brevity
+	// getters & setters omitted for brevity
 }
--- complete/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
+++ complete/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
@@ -15,39 +15,39 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 public class RelationalDataAccessApplication implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(RelationalDataAccessApplication.class);
+	private static final Logger log = LoggerFactory.getLogger(RelationalDataAccessApplication.class);
 
-    public static void main(String args[]) {
-        SpringApplication.run(RelationalDataAccessApplication.class, args);
-    }
+	public static void main(String args[]) {
+		SpringApplication.run(RelationalDataAccessApplication.class, args);
+	}
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
-    @Override
-    public void run(String... strings) throws Exception {
+	@Override
+	public void run(String... strings) throws Exception {
 
-        log.info("Creating tables");
+		log.info("Creating tables");
 
-        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE customers(" +
-                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+		jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+		jdbcTemplate.execute("CREATE TABLE customers(" +
+				"id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
 
-        // Split up the array of whole names into an array of first/last names
-        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-                .map(name -> name.split(" "))
-                .collect(Collectors.toList());
+		// Split up the array of whole names into an array of first/last names
+		List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+				.map(name -> name.split(" "))
+				.collect(Collectors.toList());
 
-        // Use a Java 8 stream to print out each tuple of the list
-        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+		// Use a Java 8 stream to print out each tuple of the list
+		splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
 
-        // Uses JdbcTemplate's batchUpdate operation to bulk load data
-        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+		// Uses JdbcTemplate's batchUpdate operation to bulk load data
+		jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
 
-        log.info("Querying for customer records where first_name = 'Josh':");
-        jdbcTemplate.query(
-                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
-                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-        ).forEach(customer -> log.info(customer.toString()));
-    }
+		log.info("Querying for customer records where first_name = 'Josh':");
+		jdbcTemplate.query(
+				"SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+				(rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+		).forEach(customer -> log.info(customer.toString()));
+	}
 }
