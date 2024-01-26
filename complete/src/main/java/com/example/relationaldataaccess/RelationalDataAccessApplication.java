a70774b209ee3b5716cd3f051c33d27647f476ff
byakuran <nwo1318@gmail.com>
2024-01-26 13:54:16 +0800

    changed "jdbcTemplate.execute("DROP TABLE customers IF EXISTS");" to "jdbcTemplate.execute("DROP TABLE IF EXISTS customers");" as stated in https://github.com/spring-guides/gs-relational-data-access/issues/27

--- complete/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
+++ complete/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
@@ -29,7 +29,7 @@ public class RelationalDataAccessApplication implements CommandLineRunner {
 
 		log.info("Creating tables");
 
-		jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+		jdbcTemplate.execute("DROP TABLE IF EXISTS customers");
 		jdbcTemplate.execute("CREATE TABLE customers(" +
 				"id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
 
